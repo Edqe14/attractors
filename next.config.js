1d217dc6a84e76d2d828d14124292be9ae833184
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-30 22:18:51 +0700

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   reactStrictMode: true,
-};
+  pwa: {
+    dest: 'public',
+    skipWaiting: true,
+    clientsClaim: true,
+  }
+});
