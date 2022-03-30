df41df8b8e8af851ddd8aa388613a4ce59e9fa38
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-30 20:02:46 +0700

    feat(seo): add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Attractors</title>
+
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta property="description" content="Chaotic Attractors Visualization" />
+        <meta property="og:title" content="Attractors" />
+        <meta property="og:site_name" content="Edqe_" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Chaotic Attractors Visualization" />
+        <meta name="theme-color" content="#111827" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <StoreProvider>
